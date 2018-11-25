41994c1f5ea847def75529c8d505e2f61a39dab8
Leonid Lukyanov <lev06191972@yandex.ru>
2018-11-26 02:16:09 +0300

    Complete task 1

--- kernel/math_functions.py
+++ kernel/math_functions.py
@@ -1,37 +1,29 @@
 import sympy as sp
+import sympy.parsing.sympy_parser as ssp
 import scipy.optimize as opt
 import scipy
 import math
 
 
-x1, x2 = sp.symbols('x1 x2')
-expr = 3*x1*x2 - x1*x2**2 - x1**2*x2
+expr = ssp.parse_expr('3*x1*x2 - x1*x2**2 - x1**2*x2')
+symbols = list(expr.free_symbols)
 
-x1_diff = sp.diff(expr, x1)
-x2_diff = sp.diff(expr, x2)
+x1_diff = sp.diff(expr, symbols[0])
+x2_diff = sp.diff(expr, symbols[1])
 
 crit = sp.solve((x1_diff, x2_diff))
 
-
 for c in crit:
-    x1_x1_diff = sp.diff(x1_diff, x1)
-    x1_x2_diff = sp.diff(x1_diff, x2)
-    x2_x2_diff = sp.diff(x2_diff, x2)
+    x1_x1_diff = sp.diff(x1_diff, symbols[0])
+    x1_x2_diff = sp.diff(x1_diff, symbols[1])
+    x2_x2_diff = sp.diff(x2_diff, symbols[1])
 
-    A = x1_x1_diff.subs({x1:c[x1], x2:c[x2]})
-    B = x1_x2_diff.subs({x1:c[x1], x2:c[x2]})
-    C = x2_x2_diff.subs({x1:c[x1], x2:c[x2]})
-
-    print(A, ' ', B, ' ', C)
+    A = x1_x1_diff.subs({symbols[0]:c[symbols[0]], symbols[1]:c[symbols[1]]})
+    B = x1_x2_diff.subs({symbols[0]:c[symbols[0]], symbols[1]:c[symbols[1]]})
+    C = x2_x2_diff.subs({symbols[0]:c[symbols[0]], symbols[1]:c[symbols[1]]})
 
     if A*C-B**2 > 0:
         if A>0:
             print('Min: ', c)
         elif A<0:
             print('Max: ', c)
-
-
-
-# print(x1_x1_diff)
-# print(x1_x2_diff)
-# print(x2_x2_diff)
